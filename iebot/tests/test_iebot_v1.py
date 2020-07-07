f60bb8741e22eb771f1dbfc9438c2a6f905b57b4
joaoavf <joao.avf@gmail.com>
2020-07-07 19:16:22 -0300

    [NEW]: Tests for horizontal summary in IEBot V1

--- iebot/tests/test_iebot_v1.py
+++ iebot/tests/test_iebot_v1.py
@@ -25,3 +25,25 @@ def test_get_vertical_summary():
                      {'top_piece': 1, 'counter': 1, 'num_spaces': 1}]
 
     assert get_vertical_summary(sample_board) == sample_result
+
+
+def test_get_row_summary():
+    assert get_row_summary([0, 0, 0, 0, 0, 0, 0]) == [0, 0]
+    assert get_row_summary([0, 0, 0, 0, 1, 0, 0]) == [1, 4]
+    assert get_row_summary([1, 1, 1, 0, 1, 1, 0]) == [3, 2]
+    assert get_row_summary([2, 2, 2, 0, 1, 1, 0]) == [3, 2]
+    assert get_row_summary([2, 2, 2, 0, 2, 2, 0]) == [3, 2]
+    assert get_row_summary([2, 1, 2, 0, 2, 2, 0]) == [2, 5]
+
+
+def test_get_horizontal_summary():
+    sample_board = np.array([[0, 0, 0, 2, 0, 0, 0],
+                             [0, 0, 1, 1, 0, 1, 1],
+                             [0, 0, 1, 2, 0, 2, 2],
+                             [0, 0, 1, 1, 0, 1, 2],
+                             [2, 0, 2, 2, 0, 1, 1],
+                             [2, 0, 1, 2, 0, 1, 2]])
+
+    sample_result = [[1, 3], [2, 3], [2, 6], [2, 3], [2, 3], [1, 0]]
+
+    assert get_horizontal_summary(sample_board) == sample_result
