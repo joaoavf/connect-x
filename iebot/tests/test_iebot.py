5dd44086e35c877b99cf51dba0d050ea7f9ac3c2
joaoavf <joao.avf@gmail.com>
2020-07-07 18:29:50 -0300

    [NEW]: Creted a test for get_vertical_summary. Also renamed the column method here for get_column_summary.

--- iebot/tests/test_iebot.py
+++ iebot/tests/test_iebot.py
@@ -1,8 +1,27 @@
 from iebot.iebot_v1 import *
 
 
-def test_get_vertical_summary():
+def test_get_column_summary():
     # Output is a tuple containing (top_piece, counter, num_spaces)
-    assert get_vertical_summary([2, 2, 2, 1, 1, 2, 2]) == (2, 3, 0)
-    assert get_vertical_summary([0, 2, 2, 1, 1, 2, 2]) == (2, 2, 1)
-    assert get_vertical_summary([0, 0, 1, 1, 1, 2, 2]) == (1, 3, 2)
+    assert get_column_summary([2, 2, 2, 1, 2, 2]) == (2, 3, 0)
+    assert get_column_summary([0, 2, 2, 1, 2, 2]) == (2, 2, 1)
+    assert get_column_summary([0, 0, 1, 1, 1, 2]) == (1, 3, 2)
+
+
+def test_get_vertical_summary():
+    sample_board = np.array([[0, 0, 0, 2, 0, 0, 0],
+                             [0, 0, 1, 1, 0, 1, 1],
+                             [0, 0, 1, 2, 0, 2, 2],
+                             [0, 0, 1, 1, 0, 1, 2],
+                             [2, 0, 2, 2, 0, 1, 1],
+                             [2, 0, 1, 2, 0, 1, 2]])
+
+    sample_result = [{'top_piece': 2, 'counter': 2, 'num_spaces': 4},
+                     {'top_piece': 0, 'counter': 0, 'num_spaces': 6},
+                     {'top_piece': 1, 'counter': 3, 'num_spaces': 1},
+                     {'top_piece': 2, 'counter': 1, 'num_spaces': 0},
+                     {'top_piece': 0, 'counter': 0, 'num_spaces': 6},
+                     {'top_piece': 1, 'counter': 1, 'num_spaces': 1},
+                     {'top_piece': 1, 'counter': 1, 'num_spaces': 1}]
+
+    assert get_vertical_summary(sample_board) == sample_result
