e73d11e88d3a37ec1fec6f4aea4bb8ff909dd223
joaoavf <joao.avf@gmail.com>
2020-07-07 21:57:35 -0300

    [FEATURE]: Fixing tests for new horizontal architecture.

--- iebot/tests/test_iebot_v1.py
+++ iebot/tests/test_iebot_v1.py
@@ -1,11 +1,12 @@
 from iebot.iebot_v1 import *
+import numpy as np
 
 
 def test_get_column_summary():
-    # Output is a tuple containing (top_piece, counter, num_spaces)
-    assert get_column_summary([2, 2, 2, 1, 2, 2]) == (2, 3, 0)
-    assert get_column_summary([0, 2, 2, 1, 2, 2]) == (2, 2, 1)
-    assert get_column_summary([0, 0, 1, 1, 1, 2]) == (1, 3, 2)
+    # Output is a tuple containing (counter, num_spaces)
+    assert get_column_summary([2, 2, 2, 1, 2, 2]) == (3, 0)
+    assert get_column_summary([0, 2, 2, 1, 2, 2]) == (2, 1)
+    assert get_column_summary([0, 0, 1, 1, 1, 2]) == (3, 2)
 
 
 def test_get_vertical_summary():
@@ -16,15 +17,11 @@ def test_get_vertical_summary():
                              [2, 0, 2, 2, 0, 1, 1],
                              [2, 0, 1, 2, 0, 1, 2]])
 
-    sample_result = [{'top_piece': 2, 'counter': 2, 'num_spaces': 4},
-                     {'top_piece': 0, 'counter': 0, 'num_spaces': 6},
-                     {'top_piece': 1, 'counter': 3, 'num_spaces': 1},
-                     {'top_piece': 2, 'counter': 1, 'num_spaces': 0},
-                     {'top_piece': 0, 'counter': 0, 'num_spaces': 6},
-                     {'top_piece': 1, 'counter': 1, 'num_spaces': 1},
-                     {'top_piece': 1, 'counter': 1, 'num_spaces': 1}]
+    sample_result = np.array([[2, 4], [0, 6], [3, 1], [1, 0], [0, 6], [1, 1], [1, 1]])
 
-    assert get_vertical_summary(sample_board) == sample_result
+    result = get_vertical_summary(sample_board)
+
+    np.testing.assert_array_equal(result, sample_result)
 
 
 def test_get_row_summary():
@@ -38,12 +35,13 @@ def test_get_row_summary():
 
 def test_get_horizontal_summary():
     sample_board = np.array([[0, 0, 0, 2, 0, 0, 0],
-                             [0, 0, 1, 1, 0, 1, 1],
+                             [0, 0, 0, 1, 0, 1, 1],
                              [0, 0, 1, 2, 0, 2, 2],
                              [0, 0, 1, 1, 0, 1, 2],
                              [2, 0, 2, 2, 0, 1, 1],
-                             [2, 0, 1, 2, 0, 1, 2]])
+                             [2, 1, 1, 1, 0, 1, 2]])
 
-    sample_result = [[1, 3], [2, 3], [2, 6], [2, 3], [2, 3], [1, 0]]
+    sample_result = np.array([[1, 3], [2, 6], [2, 6], [2, 3], [2, 3], [3, 3]])
+    result = get_horizontal_summary(sample_board)
 
-    assert get_horizontal_summary(sample_board) == sample_result
+    np.testing.assert_array_equal(result, sample_result)
