6df228a7cbde8e282250242802ab8e8d5747f373
joaoavf <joao.avf@gmail.com>
2020-07-11 02:21:59 -0300

    [BUG]: Bot was actually choosing the longer path to win and the shorter path to lose.

--- iebot/iebot/submission_v4_4.py
+++ iebot/iebot/submission_v4_4.py
@@ -102,7 +102,7 @@ def generate_plays(mask, columns_map):
 
 def negamax_ab(node, depth, columns_map, alpha=-float('inf'), beta=float('inf'), on=True):
     if depth == 0 or node.value != 0:
-        return [-node.value - (-0.01 * depth), node.play]  # Giving higher score to higher depth (less shallow)
+        return [-node.value - (0.01 * depth), node.play]  # Giving higher score to higher depth (less shallow)
     # TODO rewrite code so more depth != more shallow
 
     max_value, play = -float('inf'), -1
