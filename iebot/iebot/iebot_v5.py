382f7f73d50857388af0be3d368fe9da804acaee
joaoavf <joao.avf@gmail.com>
2020-07-12 00:09:40 -0300

    [REFACTOR]: Adjustments after refactor in utils.py, removed `columns_map` variable.

--- iebot/iebot/iebot_v5.py
+++ iebot/iebot/iebot_v5.py
@@ -7,7 +7,7 @@ from iebot.utils import *
 from iebot.tranposition_table_8_ply import tranposition_table
 
 
-def negamax_ab(node, max_depth, columns_map, alpha=-float('inf'), beta=float('inf'), root=False):
+def negamax_ab(node, max_depth, alpha=-float('inf'), beta=float('inf'), root=False):
     if max_depth == 0 or node.value != 0:
         return [-node.value - (0.01 * max_depth), node.play]  # Giving higher score to shallow nodes
     elif not root and (node.bit_board, node.mask) in tranposition_table:
@@ -15,9 +15,9 @@ def negamax_ab(node, max_depth, columns_map, alpha=-float('inf'), beta=float('in
 
     max_value, play = -float('inf'), -1
 
-    for child in node.create_children(columns_map=columns_map):
+    for child in node.create_children():
 
-        result = negamax_ab(node=child, max_depth=max_depth - 1, columns_map=columns_map, alpha=-beta, beta=-alpha)
+        result = negamax_ab(node=child, max_depth=max_depth - 1, alpha=-beta, beta=-alpha)
 
         if -result[0] > max_value:
             max_value = -result[0]
@@ -44,8 +44,8 @@ class Node:
         self.play = play
         self.value = connected_four(self.bit_board)
 
-    def create_children(self, columns_map):
-        plays = generate_plays(self.mask, columns_map)
+    def create_children(self):
+        plays = generate_plays(self.mask)
         plays = [plays.pop(i // 2) for i in reversed(range(len(plays)))]  # Order by the center
 
         for play in plays:
@@ -60,9 +60,8 @@ class Node:
 def iebot_v5(obs, config):
     board = translate_board(obs.board)
     bit_board, mask = get_position_mask_bitmap(board, obs.mark)
-    columns_map = generate_columns_map(mask)
 
     node = Node(bit_board ^ mask, mask)
 
-    _, play = negamax_ab(node=node, max_depth=8, columns_map=columns_map, root=True)
-    return transform_play_to_column(play=play, columns_map=columns_map)
+    _, play = negamax_ab(node=node, max_depth=8, root=True)
+    return transform_play_to_column(play=play)
