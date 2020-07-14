af0d775e4a3618c54ad90729779dd3e1139de63a
joaoavf <joao.avf@gmail.com>
2020-07-14 14:11:01 -0300

    [BUG]: Going back to a stable old version.

--- iebot/iebot/iebot_v6.py
+++ iebot/iebot/iebot_v6.py
@@ -6,9 +6,44 @@ This version is intended to implement transposition tables.
 from random import shuffle
 from time import time
 import math
-from iebot.utils import *
 
-external_dict = {}
+
+def manager(current_node, max_time):
+    t0 = time()
+    while time() - t0 < max_time:
+        tree_search(current_node)
+
+    scores = [child.score for child in current_node.children]
+
+    save_data_into_dict(current_node)
+
+    return current_node.children[scores.index(max(scores))].play
+
+
+def save_data_into_dict(current_node):
+    for child in current_node.children:
+        external_dict[(child.bit_board, child.mask)] = (child.score, child.count)
+        save_data_into_dict(child)
+
+
+def tree_search(node):
+    if node.value != 0 or node.mask == 279258638311359:  # Find terminal nodes
+        node.score += int(node.value)
+        node.count += 1
+        return [-node.value, node.play]  # Giving higher score to shallow nodes
+
+    child = node.explore_or_exploit()
+    result = tree_search(node=child)
+
+    node.score += result[0]
+    node.count += 1
+
+    return -result[0], child.play
+
+
+def ucb1(child, parent_count, exploration_parameter=math.sqrt(2)):
+    e1 = math.log(parent_count) / child.count
+    return (child.score / child.count) + exploration_parameter * math.sqrt(e1)
 
 
 def initialize_node(bit_board, mask):
@@ -50,68 +85,12 @@ class Node:
         return node
 
 
-def manager(current_node, max_time):
-    t0 = time()
-
-    while time() - t0 < max_time:
-        tree_search(current_node)
-
-    scores = [child.score / child.count for child in current_node.children]
-
-    save_data_into_dict(current_node)
-
-    return current_node.children[scores.index(max(scores))].play
-
-
-def save_data_into_dict(current_node):
-    for child in current_node.children:
-        external_dict[(child.bit_board, child.mask)] = (child.score, child.count)
-        save_data_into_dict(child)
-
-
-def tree_search(node):
-    if node.value != 0 or node.mask == 279258638311359:  # Find terminal nodes
-        node.score += int(node.value)
-        node.count += 1
-        return [-node.value, node.play]  # Giving higher score to shallow nodes
-
-    child = node.explore_or_exploit()
-    result = tree_search(node=child)
-
-    node.score += int(result[0] == 1)
-    node.count += 1
-
-    return -result[0], child.play
-
-
-def ucb1(child, parent_count, exploration_parameter=math.sqrt(2)):
-    e1 = math.log(parent_count) / child.count
-    return (child.score / child.count) + exploration_parameter * math.sqrt(e1)
-
-
-def iebot_v6(obs, config):
+def my_agent(obs, config):
     board = translate_board(obs.board)
     bit_board, mask = get_position_mask_bitmap(board, obs.mark)
 
-    global global_node
-    global external_dict
+    node = Node(bit_board ^ mask, mask)
 
-    try:
-        try:
-            node_selection = [child.bit_board == (bit_board ^ mask) and child.mask == mask for child in
-                              global_node.children]
-
-            global_node = global_node.children[node_selection.index(True)]
-        except:
-            scores = [child.score / child.count for child in global_node.children]
-            global_node = global_node.children[scores.index(max(scores))]
-
-            node_selection = [child.bit_board == (bit_board ^ mask) and child.mask == mask for child in
-                              global_node.children]
-            global_node = global_node.children[node_selection.index(True)]
-    except:
-        global_node = Node(bit_board ^ mask, mask)
-
-    play = manager(current_node=global_node, max_time=1)
+    play = manager(current_node=node, max_time=1)
 
     return transform_play_to_column(play=play)
